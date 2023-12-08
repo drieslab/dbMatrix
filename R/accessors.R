134cc90ba6d06afca188939cc32c873cc9f8b4df
Eddie Ruiz <32622519+Ed2uiz@users.noreply.github.com>
2023-12-08 11:26:31 -0500

    feat: update accessors and add get_con()

--- R/accessors.R
+++ R/accessors.R
@@ -4,31 +4,48 @@
 #' @param dbData
 #'
 #' @keywords internal
-get_tblName <- function(dbData){
+get_tblName <- function(dbMatrix){
   # check if dbData is a dbData object
-  if(!inherits(dbData, "dbData")){
-    stopf("dbData must be a dbData object")
+  if(!inherits(dbMatrix, "dbData")){
+    stopf("dbMatrix must be a dbData object")
   }
 
-  tblName = dbData@name
+  tblName = dbMatrix@name
 
   return(tblName)
 }
 
 #' get_dbdir
 #'
-#' @param dbData
+#' @param dbMatrix
 #'
 #' @keywords internal
 #' string of the path to the database directory
-get_dbdir <- function(dbData){
+get_dbdir <- function(dbMatrix){
   # check if dbData is a dbData object
-  if(!inherits(dbData, "dbData")){
-    stopf("dbData must be a dbData object")
+  if(!inherits(dbMatrix, "dbData")){
+    stopf("dbMatrix must be a dbData object")
   }
 
-  dbdir = dbData@con@driver@dbdir
+  dbdir = dbMatrix@value[[1]]$con@driver@dbdir
 
   return(dbdir)
 }
 
+
+#' get_con
+#'
+#' @param dbMatrix
+#'
+#' @keywords internal
+#'
+get_con <- function(dbMatrix){
+  # check if dbData is a dbData object
+  if(!inherits(dbMatrix, "dbData")){
+    stopf("dbMatrix must be a dbData object")
+  }
+
+  con = dbMatrix@value[[1]]$con
+
+  return(con)
+}
