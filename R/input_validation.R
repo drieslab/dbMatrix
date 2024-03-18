e3d6ce8a5eb0dc8bdaa3c22617b9b737dfaa915e
Ed Ruiz <ecr7407@gmail.com>
2024-03-18 09:57:44 -0400

    feat: add new input validation functions --- note: coupling functions with `dbSpatial`, may want to move to `dbData` in future

---
+++ R/input_validation.R
@@ -0,0 +1,74 @@
+#' Input validation for conn arg
+#' @param conn A \link{tbl_duckdb_connection} object
+#' @keywords internal
+.check_con <- function(conn){
+  if(missing(conn)) {
+    stop("Please provide a connection")
+  }
+
+  if(!DBI::dbIsValid(conn)) {
+    stop("Invalid connection, check your db connection.")
+  }
+
+  if(!inherits(conn, "duckdb_connection")) {
+    stop("conn must be a duckdb connection. Use duckdb drv in DBI::dbConnect()")
+  }
+}
+
+
+#' Input validation for data arg
+#' @param x A \link{Matrix}, \link{matrix}, or \link{tbl_duckdb_connection} object
+#' @keywords internal
+.check_value <- function(value){
+  is_mat = inherits(value, 'Matrix') | inherits(value, 'matrix')
+  is_duckdb = inherits(value, 'tbl_duckdb_connection')
+  is_null = is.null(value)
+  if(is.character(value)){
+    is_valid_file = file.exists(value)
+  } else {
+    is_valid_file = FALSE
+  }
+
+  if(!(is_mat | is_duckdb | is_null | is_valid_file)) {
+    stopf(
+      'Invalid "value" input passed.'
+    )
+  }
+}
+
+#' Input validation for name arg
+#' @keywords internal
+.check_name <- function(name){
+  if(missing(name)) {
+    stop("Please provide a table name")
+  }
+
+  if(!is.character(name)) {
+    stop("name must be a character string")
+  }
+
+  # if name starts with a number, add warning
+  if(grepl("^[0-9]", name)) {
+    stop("Table names should not start with a number")
+  }
+
+  # reserved name check
+  reserved_names = c("intersect", "union", "except",
+                     "select", "from", "where", "group", "by", "limit",
+                     "create", "table", "insert")
+  if(name %in% reserved_names){
+    stop("Table name cannot be a RESERVED word. Try another name.")
+  }
+}
+
+#' Input validation for overwrite arg
+#' @keywords internal
+.check_overwrite <- function(conn, overwrite, name){
+  if(!is.logical(overwrite)) {
+    stop("overwrite must be logical")
+  }
+
+  if(!overwrite & DBI::dbExistsTable(conn, name)){
+    stop("Table already exists. Set overwrite = TRUE to overwrite the table.")
+  }
+}
